982670b02cb6b68aad3479be03c5007fb8d1831e
Ivan Pavlov <alphaha@yandex.ru>
2023-12-05 11:46:32 +0300

    project

---
+++ helpers.py
@@ -0,0 +1,58 @@
+import random
+import string
+
+import conftest
+
+
+def credentials_generator():
+    def generate_random_string(length):
+        letters = string.ascii_lowercase
+        random_string = ''.join(random.choice(letters) for i in range(length))
+        return random_string
+
+    login = generate_random_string(10)
+    password = generate_random_string(10)
+    first_name = generate_random_string(10)
+
+
+    credentials = {
+        "login": login,
+        "password": password,
+        "firstName": first_name
+    }
+    return credentials
+
+
+def no_login_courier():
+    reg = credentials_generator()
+    reg.pop('login')
+    return reg
+
+
+def no_password_courier():
+    reg = credentials_generator()
+    reg.pop('password')
+    return reg
+
+
+def empty_login_courier():
+    reg = credentials_generator()
+    reg.update({'login':''})
+    return reg
+
+
+def empty_password_courier():
+    reg = credentials_generator()
+    reg.update({'password':''})
+    return reg
+
+
+def random_login_data():
+    reg = credentials_generator()
+    reg.pop('firstName')
+    return reg
+
+
+def random_track_number():
+    track_number = random.randint(1000000,9999999)
+    return track_number
